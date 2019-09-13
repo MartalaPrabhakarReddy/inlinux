562d725f5e1a7c1d9e6a6504b357d7ae62b2507b
MartalaPrabhakarReddy <44720496+MartalaPrabhakarReddy@users.noreply.github.com>
2019-09-13 15:33:30 +0530

    up

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,6 @@ class HelloWorld
     { 
         System.out.println("Hello, World"); 
         System.out.println("Hello, World1"); 
-        System.out.println("Hello, World2"); 
+        
     } 
 } 
